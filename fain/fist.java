27fa6cd229bdce42f903646f26bf109358309b1e
2002310 <2314634035@qq.com>
2021-01-03 15:56:07 +0800

    final常量的初学习

---
+++ fain/fist.java
@@ -0,0 +1,8 @@
+package fain;
+public class fist {
+    final String a = "钻石";
+    public final void nac(){
+        System.out.println("不灵不灵的");
+    }
+    // final:最终的，被final修饰的变量不可以被改变，被final修饰的方法不可以被重写，被final修饰的类不可以被继承
+}
